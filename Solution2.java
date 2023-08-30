4fb8571713a4e8ac82ba1a8919dd5ecc224ee820
Thrishank123 <thrishankkarri8@gmail.com>
2023-08-30 20:36:27 +0530

    Java Stdin and Stdout I

--- Solution2.java
+++ Solution2.java
@@ -1,11 +1,11 @@
 import java.util.Scanner;
-
 public class Solution2 {
     public static void main(String[] args) {
-         Scanner scan = new Scanner(System.in);
-        int a = scan.nextInt();
-        int b = scan.nextInt();
-        int c= scan.nextInt();
+         Scanner read= new Scanner(System.in);
+        int a = read.nextInt();
+        int b = read.nextInt();
+        int c= read.nextInt();
+        read.close();
         System.out.println(a);
         System.out.println(b);
         System.out.println(c);
